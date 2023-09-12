06c0ecbbf94f3961179c172662c1750c6fa98973
ChristyAlex2147215 <christy.alex@mca.christuniversity.in>
2023-09-12 11:00:45 +0530

    full commit

--- android/app/src/screens/Chat.js
+++ android/app/src/screens/Chat.js
@@ -1,5 +1,12 @@
 import React, {useState, useEffect, useContext} from 'react';
-import {View, FlatList, Text, TextInput, Button} from 'react-native';
+import {
+  View,
+  FlatList,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
 import {GiftedChat} from 'react-native-gifted-chat';
 import firebase from '@react-native-firebase/app';
 import auth from '@react-native-firebase/auth';
@@ -14,6 +21,7 @@ function Chat({route}) {
   const user = auth().currentUser;
   const firestoreForDefaultApp = firestore();
   const [state, setState] = useContext(AuthContext);
+  const [text, SetText] = useState('');
 
   useEffect(() => {
     const chatRef = firestoreForDefaultApp
@@ -39,6 +47,7 @@ function Chat({route}) {
   }, [chatId]);
 
   const renderMessage = ({item}) => {
+    console.log('rendering msg =>', item);
     const isSent = item.sender === state.user;
     return (
       <View
@@ -50,6 +59,7 @@ function Chat({route}) {
           padding: 10,
         }}>
         <Text>{item.text}</Text>
+        {/* <Text>{item.createdAt}</Text> */}
       </View>
     );
   };
@@ -68,7 +78,7 @@ function Chat({route}) {
   };
 
   return (
-    <View style={{flex: 1}}>
+    <View style={{flex: 1, justifyContent: 'flex-start', marginTop: 30}}>
       <FlatList
         data={messages}
         keyExtractor={item => item._id}
@@ -76,8 +86,42 @@ function Chat({route}) {
         inverted
       />
       <GiftedChat
+        messages={[]}
         onSend={newMessages => onSend(newMessages)}
         user={{_id: state.user}}
+        renderInputToolbar={() => (
+          <View
+            style={{marginLeft: 10, marginVertical: -20, flexDirection: 'row'}}>
+            <TextInput
+              style={{
+                borderWidth: 1,
+                borderColor: 'gray',
+                padding: 10,
+                width: '80%',
+              }}
+              placeholder="Type a message..."
+              onChangeText={text => SetText(text)}
+              // value={messages.length > 0 ? messages[0].text : ''}
+            />
+            <TouchableOpacity
+              style={{flex: 1, justifyContent: 'center'}}
+              onPress={() => onSend([{text}])}>
+              <Text
+                style={{
+                  backgroundColor: 'grey',
+                  alignSelf: 'center',
+                  width: '80%',
+                  height: 50,
+                  borderRadius: 25,
+                  paddingHorizontal: 15,
+                  paddingVertical: 5,
+                  fontSize: 30,
+                }}>
+                >>
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
       />
     </View>
   );
